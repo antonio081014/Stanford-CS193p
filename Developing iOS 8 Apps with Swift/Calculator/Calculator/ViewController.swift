3e94d01ad6cb5c14bc27c37df8148ac46761caad
Antonio081014 <antonio081014@antonio081014.com>
2015-06-03 22:38:41 -0700

    2014-2015 Winter Demo of Lecture 2
    Initial Commit with Demo code of Lecture 2

---
+++ Developing iOS 8 Apps with Swift/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Antonio081014 on 2/20/15.
+//  Copyright (c) 2015 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var userIsInTheMiddleOfTypingANumber:Bool = false
+
+    @IBAction func EnterDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "−": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0) }
+            default: break
+            }
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# Stanford CS193p
+
+Stanford CS193p repo has ALL the homework I worked on on the [iTunes U Course] provided by [Paul Hegaty from Stanford University]. 
+
+  * [Developing iOS 8 Apps with Swift].
+    * Homework 1, Calculator.
+  * [Developing iOS 7 Apps for iPhone and iPad].
+  
+### Version
+0.0.1
+
+
+License
+----
+
+This project is licensed under the terms of the MIT license. See the LICENSE file.
+
+[iTunes U Course]:https://itunes.apple.com/us/institution/stanford/id384228265
+[Paul Hegaty from Stanford University]:http://web.stanford.edu/class/cs193p/cgi-bin/drupal/
+[Developing iOS 8 Apps with Swift]:https://itunes.apple.com/us/course/developing-ios-8-apps-swift/id961180099
+[Developing iOS 7 Apps for iPhone and iPad]:https://itunes.apple.com/us/course/developing-ios-7-apps-for/id733644550
